4fb4bb42a53fca5cc381aaee621f745552782238
Jay Stockhausen <jay.stockhausen@gmail.com>
2023-08-07 03:37:05 +0000

    idk

--- localdrone/functions/helloworld.js
+++ localdrone/functions/helloworld.js
@@ -1,15 +1,3 @@
-export default {
-    async fetch(request) {
-    const data = {
-        hello: "world",
-    };
-
-    const json = JSON.stringify(data, null, 2);
-
-    return new Response(json, {
-        headers: {
-        "content-type": "application/json;charset=UTF-8",
-        },
-    });
-    },
-    };
+export function onRequest(context) {
+  return new Response("Hello, world!")
+}

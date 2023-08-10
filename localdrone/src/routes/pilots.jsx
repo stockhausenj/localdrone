f820eb346baadcfb409ec811b22a751125bceed0
Jay Stockhausen <jay.stockhausen@gmail.com>
2023-08-10 00:38:45 +0000

    lego

--- localdrone/src/routes/pilots.jsx
+++ localdrone/src/routes/pilots.jsx
@@ -15,20 +15,25 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import MenuItem from '@mui/material/MenuItem';
 import RefreshIcon from '@mui/icons-material/Refresh';
-import Grid from '@mui/material/Grid';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
-import Select from '@mui/material/Select';
 import Stack from '@mui/material/Stack';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { lime, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: lime,
+    secondary: purple,
+  },
+});
   
 export async function action({ request, params }) {
   let formData = await request.formData();
@@ -124,7 +129,7 @@ export default function Pilots() {
                   sx={{
                     paddingRight: 2,
                   }}
-                  inputProps={{ 'data-lpignore': true, autocomplete: "off" }}
+                  inputProps={{ 'data-lpignore': true }}
                 />
                 <TextField
                   label="Proximity (miles)"
@@ -133,7 +138,7 @@ export default function Pilots() {
                   sx={{
                     paddingRight: 2,
                   }}
-                  inputProps={{ 'data-lpignore': true, autocomplete: "off" }}
+                  inputProps={{ 'data-lpignore': true }}
                 />
               </AccordionDetails>
             </Accordion>
@@ -143,21 +148,30 @@ export default function Pilots() {
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <Typography>Service</Typography>
+                <Typography>Services</Typography>
               </AccordionSummary>
               <AccordionDetails>
-                <FormGroup>
-                  <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
-                  <FormControlLabel required control={<Checkbox />} label="Required" />
-                  <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+                <FormGroup row>
+                  <FormControlLabel control={<Checkbox />} label="Aerial Photography and Videography" />
+                  <FormControlLabel control={<Checkbox />} label="Surveying and Mapping" />
+                  <FormControlLabel control={<Checkbox />} label="Search and Rescue" />
+                </FormGroup>
+                <FormGroup row>
+                  <FormControlLabel control={<Checkbox />} label="Precision Agriculture" />
+                  <FormControlLabel control={<Checkbox />} label="Infrastructure Inspection" />
+                  <FormControlLabel control={<Checkbox />} label="Environmental Monitoring" />
                 </FormGroup>
               </AccordionDetails>
             </Accordion>
           </AccordionDetails>
         </Accordion>
-        <Button variant="contained" color="primary" startIcon={<RefreshIcon />} onClick={fetchUsers}>
-          Refresh
-        </Button>          
+        <Stack direction="row" spacing={2}>
+          <ThemeProvider theme={theme}>
+            <Button variant="contained" startIcon={<RefreshIcon />} onClick={fetchUsers}>
+              Refresh
+            </Button>     
+          </ThemeProvider>
+        </Stack>     
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 700 }} aria-label="simple table">
             <TableHead>

97f695f9deed8bc5b3b90de44f773fada9249c02
Jay Stockhausen <jay.stockhausen@gmail.com>
2023-08-26 23:26:22 +0000

    auth

--- localdrone/functions/api/auth.js
+++ localdrone/functions/api/auth.js
@@ -1,76 +1,61 @@
 import * as Realm from "realm-web";
 import bcrypt from 'bcryptjs';
+import { ResetTvOutlined, TurnedIn } from "@mui/icons-material";
 
 const {
   BSON: { ObjectId },
 } = Realm;
 
 export async function onRequest(context) {
-  const authorizationHeader = context.request.headers.get("Authorization");
+  async function mongoClient() {
+    const id = "localdrone-oajxf";
+    const config = {
+        id,
+      };
+    const app = new Realm.App(config);
+    const credentials = Realm.Credentials.apiKey(context.env.MONGODB_APIKEY);
+    const user = await app.logIn(credentials);
+    const mongo = user.mongoClient("mongodb-atlas");
 
+    return mongo;
+  }
+
+  async function verifyAuth(authEmail, authPass) {
+    const mongo = mongoClient();
+    const collection = mongo.db("localdrone").collection("users");
+    const query = { email: authEmail };
+    const projection = { email: 1, password: 1 };
+    const user = await collection.findOne(query, { projection });
+    console.log(user);
+    const match = await bcrypt.compare(authPass, user.password);
+    console.log(match);
+
+    if(match) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const authorizationHeader = context.request.headers.get("Authorization");
+  const message = {result: 'fail'};
   if (authorizationHeader) {
     const [authType, authToken] = authorizationHeader.split(" ");
 
     if (authType === "Basic") {
       const decodedToken = atob(authToken);
-      console.log("Basic token:", decodedToken);
-      const [authEmail, password] = decodedToken.split(":");
+      const [authEmail, authPass] = decodedToken.split(":");
       
-      console.log("Email:", authEmail);
-      console.log("Password:", password);
-
-      const saltRounds = 10;
-
-      const plainPassword = password;
-
-      const salt = await bcrypt.genSaltSync(saltRounds);
-
-      console.log('salt: ', salt);
- 
-      const hashedPassword = await bcrypt.hashSync(password, salt);
- 
-      console.log('Hashed Password:', hashedPassword);
+      validAuth = await verifyAuth(authEmail, authPass);
+      message.result = validAuth;
     }
   }
 
-  /*
-  const id = "localdrone-oajxf";
-  
-  const config = {
-      id,
-    };
-
-  const app = new Realm.App(config);
-
-  const credentials = Realm.Credentials.apiKey(context.env.MONGODB_APIKEY);
-
-  const user = await app.logIn(credentials);
-
-  const mongo = user.mongoClient("mongodb-atlas");
-
-  const collection = mongo.db("localdrone").collection("users");
-
-  const query = { email: authEmail };
-
-  const account = await userCollection.findOne(query);
-
-  const projection = { username: 1, zipcode: 1, missionsCompleted: 1, lastLogin: 1 };
-
-  const localdroneUsers = await collection.find({}, { projection });
-
-  const json = JSON.stringify(localdroneUsers, null, 2);
+  const json = JSON.stringify(message, null, 2);
   
   return new Response(json, {
     headers: {
       "content-type": "application/json;charset=UTF-8",
     },
   });
-  */
-  const data = {
-    hello: "world",
-  };
-
-  const json = JSON.stringify(data, null, 2);
-
-  return new Response(json);
 }
